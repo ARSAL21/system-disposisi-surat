77db4a5662399e245507f7968a281ed556eb8bc8
ARSAL21 <arsalkdm21@gmail.com>
2026-09-05 18:03:21 +0800

    feat(errors): lengkapi template respon error 401, 402, 419, 429, dan 503

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,7 @@
+﻿@extends('errors.layout')
+
+@section('code', '402')
+@section('standard_title', 'Payment Required')
+@section('punchy_message', 'KUNCI KHUSUS DIPERLUKAN')
+@section('action_prefix', 'Fitur butuh hak istimewa.')
+@section('action_button_text', 'KEMBALI KE HALAMAN SEBELUMNYA')
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,7 @@
+﻿@extends('errors.layout')
+
+@section('code', '503')
+@section('standard_title', 'Service Unavailable')
+@section('punchy_message', 'SEDANG BERSOLEK SEBENTAR')
+@section('action_prefix', 'Sistem sedang dipoles.')
+@section('action_button_text', 'KEMBALI KE HALAMAN SEBELUMNYA')
