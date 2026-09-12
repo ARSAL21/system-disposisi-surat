ac7b1184db4d7a5ebd5d387719f406bbfbc1681c
ARSAL21 <arsalkdm21@gmail.com>
2026-09-13 00:15:04 +0800

    ui(organization): perbarui penampil bagan struktur organisasi dan grid level jabatan

--- resources/js/components/back-office/organization/OrganizationChartViewer.vue
+++ resources/js/components/back-office/organization/OrganizationChartViewer.vue
@@ -731,7 +731,8 @@ const occupiedPositionsCount = computed(() => {
 
 function getLevelBadgeClass(code: string): string {
     switch (code) {
-        case 'EXECUTIVE_ENTRY':
+        case 'MAYOR':
+        case 'REGIONAL_SECRETARY':
             return 'bg-amber-500/10 text-amber-700 border-amber-500/20 dark:bg-amber-400/10 dark:text-amber-300 dark:border-amber-400/20';
         case 'ASSISTANT':
             return 'bg-indigo-500/10 text-indigo-700 border-indigo-500/20 dark:bg-indigo-400/10 dark:text-indigo-300 dark:border-indigo-400/20';
